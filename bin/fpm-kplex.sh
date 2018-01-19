7ead77caba96a7948606fbbfec23753de9fc99e7
Taylor Brockman <taylor.brockman@gmail.com>
2018-01-19 17:43:44 -0500

    DEB Iteration 4

--- bin/fpm-kplex.sh
+++ bin/fpm-kplex.sh
@@ -1,3 +1,3 @@
-fpm -s dir -t deb -n seatop-kplex -C rootfs -a armhf --version 1.3.4 --iteration 3 -d isc-dhcp-server
+fpm -s dir -t deb -n seatop-kplex -C rootfs -a armhf --version 1.3.4 --iteration 4 -d isc-dhcp-server
 mv `ls -tr *.deb` ./deb
 
